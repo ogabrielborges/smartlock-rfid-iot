ad17c9d914e796169cd5ad2f90ab472f3d9c4e71
Gabriel Borges <36484705+ogabrielborges@users.noreply.github.com>
2023-01-12 14:58:56 -0300

    Update codigo-smartlock-sinricpro.ino

--- codigo-smartlock-sinricpro.ino
+++ codigo-smartlock-sinricpro.ino
@@ -28,15 +28,14 @@
 #define RST_PIN D3
 
 // Define as credenciais do wifi e de api
-#define WIFI_SSID         "GAMINGROOM-2.4GHz"    // [PROJETO] nome da rede wifi
-#define WIFI_PASS         "entrega@newba"    // [PROJETO] senha da rede wifi
-#define APP_KEY           "4e940f2c-1789-48d1-86af-1feaab6e00b7"      // [PROJETO] Se parece com isto --> "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
-#define APP_SECRET        "43d50cea-92a7-4613-a283-a5917e86e46e-24cbd0e5-aa26-4194-b312-c406e7176f25"   // [PROJETO] Se parece com isto --> "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
-#define LOCK_ID           "63bda09942545d8b75a7acee"    // [PROJETO] Se parece com isto --> "5dc1564130xxxxxxxxxxxxxx"
-
+#define WIFI_SSID         "YOUR-WIFI-SSID"    // nome da rede wifi
+#define WIFI_PASS         "YOUR-WIFI-PASSWORD"    // senha da rede wifi
+#define APP_KEY           "YOUR-APP-KEY-SINRICPRO"      // se parece com isto --> "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "YOUR-APP-SECRET-SINRICPRO"   // se parece com isto --> "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define LOCK_ID           "YOUR-DEVICEID-SINRICPRO"    // se parece com isto --> "5dc1564130xxxxxxxxxxxxxx"
 
 // Setar a taxa de transmissão
-#define BAUD_RATE         9600                // [PROJETO] troca o baud rate
+#define BAUD_RATE         9600
 
 // Cria uma instancia para o MFRC522
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
@@ -44,101 +43,130 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 // Variável de tempo para a funcao milis
 unsigned long lock_timer = 0;
 
-void lock() { // Tranca a fechadura inteligente
+String valid_card_ids[] = { // string para registrar os cartões válidos
+    "3706D73A",
+    "031CA0F6"
+};
+
+void lock() { // tranca a fechadura inteligente
     digitalWrite(LOCK_PIN, LOW);
+    Serial.println("A fechadura foi trancada");
 }
 
-void unlock() { // Destranca a fechadura inteligente
+void unlock() { // destranca a fechadura inteligente
     digitalWrite(LOCK_PIN, HIGH);
+    Serial.println("A fechadura foi destrancada");
 }
 
-void start_lock_timer() { // Inicia a contagem do tempo na variavel lock_time
+void start_lock_timer() { // inicia a contagem do tempo na variavel lock_time
     lock_timer = millis();
+    Serial.println("Contagem para auto-lock iniciada");
 }
 
-void clear_lock_timer() { // Reseta a variavel lock_time
+void clear_lock_timer() { // reseta a variavel lock_time
     lock_timer = 0;
 }
 
-bool lock_timer_has_expired() { // Informa que a variavel lock_time chegou no seu valor maximo de X tempo
+bool lock_timer_has_expired() { // informa que a variavel lock_time chegou no seu valor maximo de X tempo
     if (lock_timer == 0) return false;
 
     unsigned long passed_time = millis() - lock_timer;
     return passed_time >= TEMP_AUTOLOCK;
 }
 
-bool RFID_card_is_present() { // Verifica e le o cartao no modulo
-    return (mfrc522.PICC_IsNewCardPresent() == true && mfrc522.PICC_ReadCardSerial() == true);
+void unlock_with_auto_relock() { // funcao que inicia o processo de destrancar e trancar a fechadura
+    unlock();
+    start_lock_timer();
 }
 
-bool RFID_card_is_valid() { // Verifica se o cartao esta cadastrado
-    char hex_buf[11]{0};
-
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-        sprintf(&hex_buf[i * 2], "%02X", mfrc522.uid.uidByte[i]);
-    }
-    String conteudo(hex_buf);
-
-    return conteudo == "3706D73A" || conteudo == "031CA0F6";
+void send_lock_state(bool state) {
+    SinricProLock &myLock = SinricPro[LOCK_ID];
+    myLock.sendLockStateEvent(state);
 }
 
-void handleRFID() {
-    if (RFID_card_is_present()) {
-        if (RFID_card_is_valid()) {
-            unlock();
-            start_lock_timer();
-            send_lock_state(false);
-        }
-      }
-}
 void auto_lock() {
     if (lock_timer_has_expired() == true) {
+        Serial.println("Tempo para auto-lock expirado");
+
         clear_lock_timer();
         lock();
         send_lock_state(true);
     }
 }
 
-void send_lock_state(bool state) { // Informa ao sinricpro o estado da fechadura
-        SinricProLock& myLock = SinricPro[LOCK_ID];
-        myLock.sendLockStateEvent(state);
+bool RFID_card_is_present() {
+    return (mfrc522.PICC_IsNewCardPresent() == true && mfrc522.PICC_ReadCardSerial() == true);
 }
 
-bool onLockState(String deviceId, bool &lockState) { // Recebe o estado da fechadura na api e executa a proxima linha
+bool RFID_card_is_not_present() {
+    return !RFID_card_is_present();
+}
+
+String get_RFID_card_ID() {
+    char hex_buf[11]{0};
+
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        sprintf(&hex_buf[i * 2], "%02X", mfrc522.uid.uidByte[i]);
+    }
+
+    return String(hex_buf);
+}
+
+bool validate_RFID_card(String card_id) {
+    for (auto &valid_id : valid_card_ids) {
+        if (card_id == valid_id) return true;
+    }
+
+    return false;
+}
+
+void handleRFID() {
+    if (RFID_card_is_not_present()) return;
+
+    String card_id            = get_RFID_card_ID();
+    bool   RFID_card_is_valid = validate_RFID_card(card_id);
+
+    if (RFID_card_is_valid) {
+        Serial.printf("O cartao RFID \"%s\" e valido.\r\n", card_id.c_str());
+
+        unlock_with_auto_relock();
+        send_lock_state(false);
+    } else {
+        Serial.printf("O cartao RFID \"%s\" nao e valido.\r\n", card_id.c_str());
+    }
+}
+
+bool onLockState(String deviceId, bool &lockState) { // recebe o estado da fechadura na api e executa a proxima linha
     if (lockState == false)
   {
-    Serial.printf("A fechadura inteligente foi %s\r\n", lockState?"travada":"destravada"); // informa no monitor serial o estado da fechadura
-    unlock();
-    start_lock_timer();
-// Descomente esse código se sua fechadura vai ser acionada várias vezes em um curto período de tempo para não sobrecarregar a api do SinricPro
-//    lockState = true;
+        unlock_with_auto_relock();
     } else {
         lock();
   } 
   return true;
 }
 
-void setupWiFi() { // Funcao de setup do Wifi
-    Serial.printf("\r\n[Wifi]: Conectando");
+void setupWiFi() { // funcao de setup do Wifi
+    Serial.printf("[WIFI] Conectando\r\n");
     WiFi.begin(WIFI_SSID, WIFI_PASS);
 
     while (WiFi.status() != WL_CONNECTED) {
-        Serial.printf(".");
+        Serial.printf("-");
         delay(250);
     }
     Serial.printf("\r\n[CONECTADO] Endereco IP: %s\r\n", WiFi.localIP().toString().c_str());
 }
 
-void setupRFID() { // Funcao de setup do MFRC522
+void setupRFID() { // funcao de setup do MFRC522
     SPI.begin();
     mfrc522.PCD_Init();
 }
 
-void setupLock() { // Define o pino  da fechadura como SAIDA
+void setupLock() { // define o pino  da fechadura como SAIDA
     pinMode(LOCK_PIN, OUTPUT);
 }
 
-void setupSinricPro() { // Funcao de setup do SinricPro
+void setupSinricPro() { // funcao de setup do SinricPro
     SinricProLock &myLock = SinricPro[LOCK_ID];
     myLock.onLockState(onLockState);
     SinricPro.onConnected([]() { Serial.printf("Conectado ao SinricPro\r\n"); });
